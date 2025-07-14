2e939130d2858233ca16dd11a53cae895db20507
MuammerKRKS <muhde99.mk@gmail.com>
2025-07-15 00:06:04 +0300

    Add files via upload

---
+++ artik-yil-hesaplama/src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Yıl giriniz: ");
+int year = scanner.nextInt();
+
+      if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+    System.out.println(year + " Artık yıldır. ");}
+      else {
+          System.out.println(year + " Artık yıl değildir. ");}
+        }
+    }
