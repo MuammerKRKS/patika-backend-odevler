f1f540437a6f2a4757e437acab63ec9d9600a510
MuammerKRKS <muhde99.mk@gmail.com>
2025-07-16 17:31:59 +0300

    Add files via upload

---
+++ aritmetik-islemler/src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+       //sayılar kullanıcan girilmediği için scanner kullanmadım
+        int a = 10;
+        int b = 2;
+        int c = 3;
+
+        int result = a + b * c- b;
+
+        System.out.println("sonuc: " + result);
+
+        }
+    }
