19088783cc70e8933a729afcdde6d207402c06d4
MuammerKRKS <muhde99.mk@gmail.com>
2025-07-16 20:26:50 +0300

    Update Main.java

--- aritmetik-islemler/src/main/java/org/example/Main.java
+++ aritmetik-islemler/src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-       //sayılar kullanıcan girilmediği için scanner kullanmadım
+       //sayılar kullanıcıdan girilmediği için scanner kullanmadım
         int a = 10;
         int b = 2;
         int c = 3;
